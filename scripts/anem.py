93c94c9a1a3513b08f158c859aa4deea36f81ab5
ikertesz <ikertesz@student.ethz.ch>
2019-01-29 19:24:02 +0000

    running ros node for wind sensor

--- scripts/anem.py
+++ scripts/anem.py
@@ -38,62 +38,66 @@ def windspeedFromDiffPressure(dp) :
     p0=966 # mbar calib pressure
     t0=298.15 # calib temperature
  
-
-#np.set_printoptions(precision=4)
-
-channel1 = rospy.Publisher('anem_data1', Float64, queue_size=10)
-channel2 = rospy.Publisher('anem_data2', Float64, queue_size=10)
-channel2 = rospy.Publisher('anem_data3', Float64, queue_size=10)
-rospy.init_node('anem', anonymous=True)
-rate = rospy.Rate(3) # 3hz
-
-rospy.loginfo('Opening i2c SMBus')
-bus=smbus.SMBus(1) #The default i2c bus
-i2cAddr=(0x21,0x22,0x23)
-
-rospy.loginfo('Stopping existing continuous measurements')
-for a in i2cAddr:
-    bus.write_i2c_block_data(a, 0x3F, [0xF9]) #Stop any cont measurement of the sensor
-
-time.sleep(0.8)
-
-#Start Continuous Measurement (5.3.1 in Data sheet)
-rospy.loginfo('Starting continuous measurement (5.3.1 in Data sheet)')
-
-##Command code (Hex)        Temperature compensation            Averaging
-##0x3603                    Mass flow                           Average  till read
-##0x3608                    Mass flow None                      Update rate 0.5ms
-##0x3615                    Differential pressure               Average till read
-##0x361E                    Differential pressure None          Update rate 0.5ms
-
-for a in i2cAddr:
-    bus.write_i2c_block_data(a, 0x36, [0X15]) 
-
-time.sleep(.1)
-
-rospy.loginfo("Initialization completed")
-
-
 def anem():
+
+    #np.set_printoptions(precision=4)
+
+    channel1 = rospy.Publisher('anem_data1', Float64, queue_size=10)
+    channel2 = rospy.Publisher('anem_data2', Float64, queue_size=10)
+    channel3 = rospy.Publisher('anem_data3', Float64, queue_size=10)
+    rospy.init_node('anem', anonymous=True)
+    rate = rospy.Rate(3) # 3hz
+
+    rospy.loginfo('Opening i2c SMBus')
+    bus=smbus.SMBus(1) #The default i2c bus
+    i2cAddr=(0x21,0x22,0x23)
+
+    rospy.loginfo('Stopping existing continuous measurements')
+    for a in i2cAddr:
+        bus.write_i2c_block_data(a, 0x3F, [0xF9]) #Stop any cont measurement of the sensor
+
+    time.sleep(0.8)
+
+    #Start Continuous Measurement (5.3.1 in Data sheet)
+    rospy.loginfo('Starting continuous measurement (5.3.1 in Data sheet)')
+
+    ##Command code (Hex)        Temperature compensation            Averaging
+    ##0x3603                    Mass flow                           Average  till read
+    ##0x3608                    Mass flow None                      Update rate 0.5ms
+    ##0x3615                    Differential pressure               Average till read
+    ##0x361E                    Differential pressure None          Update rate 0.5ms
+
+    for a in i2cAddr:
+        bus.write_i2c_block_data(a, 0x36, [0X15]) 
+
+    time.sleep(.1)
+
+    rospy.loginfo("Initialization of anem wind sensor completed")
+
     dp=list()
+
     try:
-        i=0
-        del dp[:]
-        for a in i2cAddr:
-            b=bus.read_i2c_block_data(a,0,9)
-    	v=int_from_bytes([b[0],b[1]])
-    	#print(b,v)
-        dp.append(v/240.)
-        rospy.loginfo("ch1: %f, ch2: %f, ch3: %f",dp(0),dp(1),dp(2))
-        # print [" %8.4f" % v for v in dp]
-        channel1.publish(dp(0))
-        channel2.publish(dp(1))
-        channel3.publish(dp(2))
+        while not rospy.is_shutdown():
+            del dp[:]
+            for a in i2cAddr:
+                b=bus.read_i2c_block_data(a,0,9)
+                v=int_from_bytes([b[0],b[1]])
+                dp.append(v/240.)
+            rospy.loginfo("ch1: %f, ch2: %f, ch3: %f",dp[0],dp[1],dp[2])
+            # print [" %8.4f" % v for v in dp]
+            channel1.publish(dp[0])
+            channel2.publish(dp[1])
+            channel3.publish(dp[2])
+            rate.sleep()
     except KeyboardInterrupt:
-        GPIO.cleanup()
+            rospy.loginfo('Stopping existing continuous measurements')
+            for a in i2cAddr:
+                bus.write_i2c_block_data(a, 0x3F, [0xF9]) #Stop any cont measurement of the sensor
 
 if __name__ == '__main__':
     try:
         anem()
     except rospy.ROSInterruptException:
-        pass
+        rospy.loginfo('Stopping existing continuous measurements')
+        for a in i2cAddr:
+            bus.write_i2c_block_data(a, 0x3F, [0xF9]) #Stop any cont measurement of the sensor
