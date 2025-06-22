340f50867ed597ab1e691ff664fd0712da8b927b
Tobi Delbruck <tobi@ini.uzh.ch>
2025-06-22 10:59:46 +0000

    migrated imu.py to ROS2 and gemini made a Makefile for RTIMULib2 plus python wrapper build

--- scripts/imu.py
+++ scripts/imu.py
@@ -1,127 +1,122 @@
 #!/usr/bin/env python
-
-#Reading the Sensirion SDP32 sensor
-#Dev by JJ SlabbertSDP810_example / modified by tobi
-#Code tested with Python 2.7
-#Run sudo i2cdetect -y 1 in the terminal, to see if the sensor is connected. it will show address 25
-#Check the datasheet at https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/0_Datasheets/Differential_Pressure/Sensirion_Differential_Pressure_Sensors_SDP8xx_Digital_Datasheet.pdf
-#The sensor i2c address is 0x21 to 0x23 (Not user programable).
+# ROS2 version of imu.py
+# Reads the RTIMU sensor and publishes pose and raw sensor data.
 
 # topics
 # publishes
 # /pose, Vector3 .z is compass heading in degrees corrected by magnetometer calibration
-# /accel raw sensor values
-# /gyro raw sensor values
-# /compass raw sensor values
+# /accel raw sensor values in g
+# /gyro raw sensor values in deg/s
+# /compass raw sensor values in uT
 
-import rospy
+import rclpy
+from rclpy.node import Node
 import RTIMU
 import os.path
 from  geometry_msgs.msg import Vector3
-import smbus
 import time
-# import numpy as np
 import math
 
- 
-def imu():
+class ImuNode(Node):
+    def __init__(self):
+        super().__init__('imu_node')
+        self.get_logger().info('Initializing IMU node...')
 
-    print('initialiing IMU ROS node')
-    rospy.init_node('imu', anonymous=True,log_level=rospy.INFO)
-    #np.set_printoptions(precision=4)
+        SETTINGS_FILE = "RTIMULib"
 
-    SETTINGS_FILE = "RTIMULib"
+        abs_path = os.path.abspath(SETTINGS_FILE + ".ini")
+        self.get_logger().info(f"Using IMU settings file: {abs_path}")
+        if not os.path.exists(SETTINGS_FILE + ".ini"):
+            self.get_logger().info("Settings file does not exist, will be created")
 
-    abs_path=os.path.abspath(SETTINGS_FILE+".ini")
-    rospy.loginfo("Using IMU settings file " + abs_path)
-    if not os.path.exists(SETTINGS_FILE + ".ini"):
-      rospy.loginfo("Settings file does not exist, will be created")
+        s = RTIMU.Settings(SETTINGS_FILE)
+        self.imu = RTIMU.RTIMU(s)
 
-    s = RTIMU.Settings(SETTINGS_FILE)
-    print('Settings are '+str(s))
-    imu = RTIMU.RTIMU(s)
+        self.get_logger().info(f"IMU Name: {self.imu.IMUName()}")
 
-    rospy.loginfo("IMU Name: " + imu.IMUName())
+        if not self.imu.IMUInit():
+            self.get_logger().error("************* IMU Init Failed, shutting down. *************")
+            # In a class-based node, we can't just quit().
+            # We'll prevent the timer from being created and let the main function exit.
+            self.destroy_node()
+            rclpy.shutdown()
+            return
+        else:
+            self.get_logger().info("IMU Init Succeeded")
 
-    if (not imu.IMUInit()):
-        rospy.logerr("*************   IMU Init Failed, quiting imu.py")
-        quit(1)
-    else:
-        rospy.loginfo("IMU Init Succeeded")
+        # Set fusion parameters
+        self.imu.setSlerpPower(0.02)
+        self.imu.setGyroEnable(True)
+        self.imu.setAccelEnable(True)
+        self.imu.setCompassEnable(True)
 
-    # this is a good time to set any fusion parameters
+        # Publishers
+        self.pub_accel = self.create_publisher(Vector3, 'accel', 10)
+        self.pub_gyro = self.create_publisher(Vector3, 'gyro', 10)
+        self.pub_compass = self.create_publisher(Vector3, 'compass', 10)
+        self.pub_pose = self.create_publisher(Vector3, 'pose', 10)
 
-    imu.setSlerpPower(0.02)
-    imu.setGyroEnable(True)
-    imu.setAccelEnable(True)
-    imu.setCompassEnable(True)
+        # Main loop timer
+        self.timer = self.create_timer(0.1, self.timer_callback) # 10 Hz
 
-    #pub_pose = rospy.Publisher('pose', Vector3, queue_size=100)
-    pub_accel = rospy.Publisher('accel', Vector3, queue_size=100)
-    pub_gyro = rospy.Publisher('gyro', Vector3, queue_size=100)
-    pub_compass = rospy.Publisher('compass', Vector3, queue_size=100)
-    pub_pose = rospy.Publisher('pose', Vector3, queue_size=100)
-   
-    rate = rospy.Rate(10) # Hz
+    def timer_callback(self):
+        if self.imu.IMURead():
+            data = self.imu.getIMUData()
 
-    try:
-        while not rospy.is_shutdown():
-            if imu.IMURead():
-                # x, y, z = imu.getFusionData()
-                # print("%f %f %f" % (x,y,z))
-                data = imu.getIMUData()
+            # Sample of data returned:
+            # {'accelValid': True, 'fusionQPoseValid': True, 'timestamp': ...,
+            #  'compassValid': True, 'compass': (-24.0, 20.3, 2.3), # uT
+            #  'accel': (-0.4, 0.8, 0.3), # g
+            #  'gyroValid': True, 'gyro': (0.006, 0.014, -0.004), # rad/s
+            #  'fusionQPose': (0.04, 0.29, -0.5, -0.8),
+            #  'fusionPoseValid': True, 'fusionPose': (1.2, 0.4, -2.7)} # rad
 
-                """ 
-                sample of data returned
+            fusionPose = data.get("fusionPose", (0.0, 0.0, 0.0))
+            gyro = data.get('gyro', (0.0, 0.0, 0.0))
+            accel = data.get('accel', (0.0, 0.0, 0.0))
+            compass = data.get('compass', (0.0, 0.0, 0.0))
 
-                {'accelValid': True, 'fusionQPoseValid': True, 'timestamp': 1744717994965783L, 
-                'compassValid': True, 
-                'compass': (-24.0, 20.340002059936523, 2.3400001525878906), (should be uT (micro Tesla)? )
-                'accel': (-0.4088134765625, 0.838134765625, 0.34693604707717896), (units shold be g)
-                'gyroValid': True, 
-                'gyro': (0.0062618679367005825, 0.0140159260481596, -0.004969525150954723), (should be rad/s)
-                'fusionQPose': (0.04537138342857361, 0.2931070029735565, -0.5019389986991882, -0.8124573230743408), 
-                'fusionPoseValid': True, 
-                'fusionPose': (1.2032440900802612, 0.4452976584434509, -2.7216269969940186)} (should be rad)
-                """
-#                rospy.logdebug(data)
-                fusionPose = data["fusionPose"]
-                gyro=data['gyro']
-                accel=data['accel']
-                compass=data['compass']
+            # Convert radians to degrees for pose and gyro
+            rollDeg = math.degrees(fusionPose[0])
+            pitchDeg = math.degrees(fusionPose[1])
+            yawDeg = math.degrees(fusionPose[2])
 
-                rollDeg=math.degrees(fusionPose[0])
-                panDeg=math.degrees(fusionPose[1])
-                yawDeg=math.degrees(fusionPose[2])
-                
-                gx=math.degrees(gyro[0])
-                gy=math.degrees(gyro[1])
-                gz=math.degrees(gyro[2])
-                
-                ax=accel[0]
-                ay=accel[1]
-                az=accel[2]
+            gx_dps = math.degrees(gyro[0])
+            gy_dps = math.degrees(gyro[1])
+            gz_dps = math.degrees(gyro[2])
 
-                cx=compass[0]
-                cy=compass[1]
-                cz=compass[2]
+            self.get_logger().debug(
+                f"imu: pose rpy=({rollDeg:.2f},{pitchDeg:.2f},{yawDeg:.2f}), "
+                f"gyro xyz=({gx_dps:.1f},{gy_dps:.1f},{gz_dps:.1f})deg/s, "
+                f"acc xyz=({accel[0]:.2f},{accel[1]:.2f},{accel[2]:.2f})g, "
+                f"mag xyz=({compass[0]:.1f},{compass[1]:.1f},{compass[2]:.1f})uT"
+            )
 
-                rospy.logdebug("imu: pose rpy=(%.2f,%.2f,%.2f), gyro xyz=(%.1f,%.1f,%.1f)deg/s, acc xyz=(%.2f,%.2f,%.2f)g, mag xyz=(%.1f,%.1f,%.1f)uT" 
-                    % (rollDeg,panDeg,yawDeg,gx,gy,gz, ax,ay,az, cx,cy,cz)) 
-                pub_compass.publish(Vector3(cx,cy,cz))
-                pub_gyro.publish(Vector3(gx,gy,gz))
-                pub_accel.publish(Vector3(ax,ay,az))
-                pub_pose.publish(Vector3(rollDeg,panDeg,yawDeg))
-            else:
-                rospy.logerr('could not read IMU')
+            # Publish compass data (uT)
+            self.pub_compass.publish(Vector3(x=compass[0], y=compass[1], z=compass[2]))
+            # Publish gyro data (deg/s)
+            self.pub_gyro.publish(Vector3(x=gx_dps, y=gy_dps, z=gz_dps))
+            # Publish accel data (g)
+            self.pub_accel.publish(Vector3(x=accel[0], y=accel[1], z=accel[2]))
+            # Publish pose data (roll, pitch, yaw in degrees)
+            self.pub_pose.publish(Vector3(x=rollDeg, y=pitchDeg, z=yawDeg))
+        else:
+            self.get_logger().error('Could not read IMU')
 
-            rate.sleep()
-    except KeyboardInterrupt:
-            rospy.loginfo('Stopping IMU existing continuous measurements')
+def main(args=None):
+    rclpy.init(args=args)
+    imu_node = ImuNode()
 
+    if rclpy.ok():
+        try:
+            rclpy.spin(imu_node)
+        except KeyboardInterrupt:
+            print("Keyboard interrupt, stopping IMU node.")
+        finally:
+            # The RTIMU library doesn't seem to have a specific de-init function.
+            # Destroying the node and shutting down rclpy is sufficient.
+            imu_node.destroy_node()
+            # rclpy.shutdown() is not called here to avoid the "context already shutdown" error.
 
 if __name__ == '__main__':
-    try:
-        imu()
-    except rospy.ROSInterruptException:
-        rospy.loginfo('Stopping IMU existing continuous measurements')
+    main()
